7812db2df10aaefca909103619dcb415b4e73e8f
Clay Schick <clay.schick@gmail.com>
2020-11-28 10:09:39 -0600

    Added hardware behavior adapters

--- fw/config/test.exs
+++ fw/config/test.exs
@@ -1,4 +1,13 @@
-use Mix.Config
+import Config
+
+config :fw, Fw.Temperature,
+  spi_adapter: Fw.Adapters.SpiTest
+
+config :fw, Fw.Fan,
+  pwm_adapter: Fw.Adapters.PwmTest,
+  pwm_pin: 18,
+  pwm_frequency: 25_000,
+  pwm_frequency_multiplier: 1000
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
---
+++ fw/lib/fw/adapters/spi_behaviour.ex
@@ -0,0 +1,8 @@
+defmodule Fw.Adapters.SpiBehaviour do
+  @moduledoc """
+  Specification for SPI behaviour
+  """
+  @callback open(device :: String.t, options :: charlist) :: {:ok, reference}
+
+  @callback transfer(ref :: reference, data :: binary) :: {:ok, binary}
+end
